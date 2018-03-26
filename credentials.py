0f5bce1f8f678df2a9716c2bb68ecf2032be6b82
zhangna <1376719595@qq.com>
2018-03-26 16:37:21 +0800

    使用python分析trump's tweets

---
+++ credentials.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# ### Twitter App access keys for @user
+
+# In[1]:
+
+
+CONSUMER_KEY = 'Dou1376719595'
+CONSUMER_SECRET = 'zhang1zhu9'
+
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
+
